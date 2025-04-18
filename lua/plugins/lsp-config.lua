09451fcfd6a38092af2209d352a36e6d426c0ef3
D33pUnd3r <d33pund3r@pop-os.eduroam.gmu.edu>
2025-04-18 11:18:30 -0400

    personalize linux

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -3,7 +3,7 @@ return {
     "williamboman/mason.nvim",
     config = function()
       require("mason").setup({
-        ensure_installed = { "lua_ls", "clangd", "jedi-language-server" },
+        ensure_installed = { "lua_ls", "clangd"},
       })
     end,
   },
@@ -11,7 +11,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "clangd", "pylsp", "basedpyright" },
+        ensure_installed = { "lua_ls", "clangd", "pylsp"},
       })
     end,
   },
@@ -20,57 +20,16 @@ return {
     config = function()
       local lsp = require("lspconfig")
       lsp.lua_ls.setup({})
-      lsp.pylsp.setup({
-        settings = {
-          pylsp = {
-            plugins = {
-              pycodestyle = {
-                ignore = { "W391" },
-                maxLineLength = 150,
-              },
-            },
-          },
-        },
-      })
-
-      require("lspconfig").basedpyright.setup({
-        settings = {
-          basedpyright = {
-            analysis = {
-              useLibraryCodeForTypes = true,
-              typeCheckingMode = "basic",
-              diagnosticMode = "workspace",
-              autoSearchPath = true,
-              inlayHints = {
-                callArgumentNames = true,
-              },
-              extraPaths = {
-                "...",
-                "...",
-              },
-            },
-            python = {
-              venvPath = "/path/to/venv",
-              venv = "venv",
-            },
-          },
-        },
-      })
-
-      lsp.clangd.setup({
-        cmd = {
-          "C://MinGW//bin//clangd.exe",
-          "--query-driver=C://MinGW//bin//g++.exe",
-          "--clang-tidy",
-          "--background-index",
-          "--completion-style=detailed",
-        },
-        filetypes = { "c", "cpp", "objc", "objcpp" },
-      })
+      lsp.pylsp.setup({})
+      lsp.clangd.setup({})
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
       vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
       vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.diagnostic.config({
+        virtual_text = true,
+        virtual_line = true,
+      })
     end,
   },
 }
